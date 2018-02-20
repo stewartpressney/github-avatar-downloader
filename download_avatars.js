2023c1d92c83b1f96d52a08dc239aff1d3bdc02d
stewartpressney <stewartpressney@gmail.com>
2018-02-19 19:48:43 -0800

    step 4 commit

--- download_avatars.js
+++ download_avatars.js
@@ -4,7 +4,10 @@ console.log('Welcome to the GitHub - Avatar Downloader')
 
 
 function getRepoContributors (repoOwner, repoName, cb) {
-
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body){
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
