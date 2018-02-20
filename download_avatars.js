89208aaf5f603bc40e677070b5535291eb448b41
stewartpressney <stewartpressney@gmail.com>
2018-02-19 20:44:30 -0800

    step 6 commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,33 @@
 var request = require('request');
 
+var secret = require('./secrets');
+
 console.log('Welcome to the GitHub - Avatar Downloader')
 
-
 function getRepoContributors (repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body){
-    cb(err, body);
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'stewartpressney',
+      'Authorization' : 'token '+secret.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body){
+    if (err) {
+      throw err;
+    }
+    console.error(res);
+    var parsedJSON = JSON.parse(body)
+    cb(parsedJSON);
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function(results) {
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+    for(result of results){
+      console.log(result['avatar_url'])
+    }
+
 });
